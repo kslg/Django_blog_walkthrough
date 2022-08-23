4ca34c900f98624fc36ce9d10bd8baba088ad02b
Krishan <kslg257@gmail.com>
2022-08-23 14:11:17 +0000

    Building The Admin Site - part 2

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
 from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
-# from django_summernote.admin import SummernoteModelAdmin
+
 
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    summernote_fields = ('content',)
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    summernote_fields = ('content')
+    search_fields = ['title', 'content']
+
+
+@admin.register(Comment)
+class CommentAdmin (admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ['name', 'email', 'body']
+    actions = ['approved_comments']
+
+    def approved_comments (self, request, queryset):
+        queryset.update(approved=True)
