606f0cd97acb8b31b550250148ffd1a18b0fcc24
Krishan <kslg257@gmail.com>
2022-08-23 06:53:29 +0000

    Building admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+# from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content',)
