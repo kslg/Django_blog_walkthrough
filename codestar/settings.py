8dff8fd49792aaa15be0cd13c2bcf3bee0d611f6
Krishan <kslg257@gmail.com>
2022-08-25 17:53:23 +0000

    update requirements.txt and settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECREY_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
