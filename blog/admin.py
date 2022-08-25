219fa7ffe6e8575225e03b074314b018c81865eb
Krishan <kslg257@gmail.com>
2022-08-25 16:39:51 +0000

    Creating views.

--- blog/admin.py
+++ blog/admin.py
@@ -7,12 +7,12 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    prepopulated_fields = {'slug': ('title',)}
-    list_filter = ('status', 'created_on')
     list_display = ('title', 'slug', 'status', 'created_on')
-    summernote_fields = ('content')
+    list_filter = ('status', 'created_on')
     search_fields = ['title', 'content']
-
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content')
+    
 
 @admin.register(Comment)
 class CommentAdmin (admin.ModelAdmin):
--- codestar/settings.py
+++ codestar/settings.py
@@ -19,6 +19,7 @@ if os.path.isfile("env.py"):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -31,7 +32,7 @@ SECRET_KEY = os.environ.get('SECREY_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["codestar-project-walkthrough.herokuapp.com", "localhost"]
 
 
 # Application definition
@@ -62,7 +63,7 @@ ROOT_URLCONF = 'codestar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,7 +129,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
