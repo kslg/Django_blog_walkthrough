03cc10570d58182ffaaa05e3489639250476767f
Krishan <kslg257@gmail.com>
2022-08-27 11:09:26 +0000

    updated settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'django_summernote',
     'blog',
