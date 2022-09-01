b7d49253a8a55e0968534e909860c331676f16aa
Krishan <kslg257@gmail.com>
2022-09-01 20:35:50 +0000

    deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ["codestar-project-walkthrough.herokuapp.com", "localhost"]
 
